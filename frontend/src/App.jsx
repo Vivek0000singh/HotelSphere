aa07321bd872db3bf233b358e7391aec21196869
Vivek0000singh <viveksingh.orion.78@gmail.com>
2026-01-20 17:35:48 +0530

    footer added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,20 +7,28 @@ import RoomDetails from "./pages/RoomDetails";
 import BookingForm from "./pages/BookingForm";
 import MyBookings from "./pages/MyBookings";
 import AdminPanel from "./pages/AdminPanel";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rooms" element={<Rooms />} />
-        <Route path="/rooms/:id" element={<RoomDetails />} />
-        <Route path="/book/:roomId" element={<BookingForm />} />
-        <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
+      <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
+        <div style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms" element={<Rooms />} />
+            <Route path="/rooms/:id" element={<RoomDetails />} />
+            <Route path="/book/:roomId" element={<BookingForm />} />
+            <Route path="/my-bookings" element={<MyBookings />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin" element={<AdminPanel />} />
+          </Routes>
+        </div>
+
+        {/* Footer sits naturally at the bottom now, with no white gap above it */}
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
